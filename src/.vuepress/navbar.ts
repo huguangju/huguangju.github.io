4e40fd6c6489baf27113510ff09465fea1074cff
huguangju <huguangju@weli.cn>
2023-03-24 19:18:40 +0800

    update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,6 +4,6 @@ export default navbar([
   "/",
   { text: "Vue", icon: "vue", link: "/vue/" },
   { text: "React", icon: "react", link: "/react/" },
-  { text: "导航", icon: "read", link: "/nav/" },
-  { text: "文档", icon: "read", link: "/doc/" },
+  // { text: "文档", icon: "read", link: "/doc/" },
+  { text: "资源", icon: "expansion", link: "/nav/" },
 ]);
---
+++ src/engineering/README.md
@@ -0,0 +1,5 @@
+---
+icon: repair
+title: 工程化
+article: false
+---
