98275beb4f997980b3827cf3dbf8562bb41da253
huguangju <huguangju@weli.cn>
2023-04-05 20:34:43 +0800

    add search support

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme";
 
 export default defineUserConfig({
@@ -9,6 +10,27 @@ export default defineUserConfig({
 
   lang: "zh-CN",
   theme,
+
+  plugins: [
+    // https://plugin-search-pro.vuejs.press/zh/config.html
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 指定热键
+      hotKeys: [{ key: 'k', meta: true }],
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category as string,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag as string,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 
   // Enable it with pwa
   // shouldPrefetch: false,
